14a4918d2431e5407d082db7044fa09bf0471143
valeriogonini <valeriogonini@gmail.com>
2024-05-09 15:29:44 +0200

    add foreach

--- index.php
+++ index.php
@@ -16,6 +16,19 @@ class Producion
     }
 }
 
+$harry_potter = new Producion('Harry Potter', 9, 'English');
+$films[] = $harry_potter;
+
+$kill_bill = new Producion('Kill Bill', 9.5, 'English');
+$films[] = $kill_bill;
+
+$il_gladiatore = new Producion('Il Gladiatore', 7, 'Italiano');
+$films[] = $il_gladiatore;
+
+$avengers = new Producion('Avengers', 8.5, 'English');
+$films[] = $avengers;
+
+
 ?>
 
 
@@ -27,6 +40,7 @@ class Producion
 
 <head>
     <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
@@ -35,7 +49,16 @@ class Producion
     <div class="container">
         <div class="row">
             <div class="col-3">
+                <?php
+                foreach ($films as $film) {
+                ?>
+                    <p><?php echo $film->title ?> </p>
+                    <p><?php echo $film->vote ?> </p>
+                    <p><?php echo $film->language ?> </p>
 
+                <?php
+                }
+                ?>
             </div>
         </div>
     </div>
