73e70b27fc146cd2d09786597639a7f215e747f9
valeriogonini <valeriogonini@gmail.com>
2024-05-09 15:11:09 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+$films = [];
+
+class Producion
+{
+    public $title;
+    public $vote;
+    public $language;
+
+    function __construct($_title, $_vote, $_language)
+    {
+        $this->title = $_title;
+        $this->vote = $_vote;
+        $this->language = $_language;
+    }
+}
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
