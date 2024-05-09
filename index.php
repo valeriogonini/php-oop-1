f15b76b984f05f0df15b3bc3da433017fba07f8e
valeriogonini <valeriogonini@gmail.com>
2024-05-09 15:59:51 +0200

    add img

--- index.php
+++ index.php
@@ -7,25 +7,28 @@ class Producion
     public $title;
     public $vote;
     public $language;
+    public $img;
 
-    function __construct($_title, $_vote, $_language)
+
+    function __construct($_title, $_vote, $_language, $_img)
     {
         $this->title = $_title;
         $this->vote = $_vote;
         $this->language = $_language;
+        $this->img = $_img;
     }
 }
 
-$harry_potter = new Producion('Harry Potter', 9, 'English');
+$harry_potter = new Producion('Harry Potter', 9, 'English', 'https://image.tmdb.org/t/p/w342//wuMc08IPKEatf9rnMNXvIDxqP4W.jpg');
 $films[] = $harry_potter;
 
-$kill_bill = new Producion('Kill Bill', 9.5, 'English');
+$kill_bill = new Producion('Kill Bill', 9.5, 'English', 'https://image.tmdb.org/t/p/w342//v7TaX8kXMXs5yFFGR41guUDNcnB.jpg');
 $films[] = $kill_bill;
 
-$il_gladiatore = new Producion('Il Gladiatore', 7, 'Italiano');
+$il_gladiatore = new Producion('Il Gladiatore', 7, 'Italiano', 'https://image.tmdb.org/t/p/w342//ty8TGRuvJLPUmAR1H1nRIsgwvim.jpg');
 $films[] = $il_gladiatore;
 
-$avengers = new Producion('Avengers', 8.5, 'English');
+$avengers = new Producion('Avengers', 8.5, 'English', 'https://image.tmdb.org/t/p/w342//RYMX2wcKCBAr24UyPD7xwmjaTn.jpg');
 $films[] = $avengers;
 
 
@@ -51,6 +54,7 @@ $films[] = $avengers;
         <thead>
             <tr>
                 <th scope="col">Film</th>
+                <th scope="col">Copertina</th>
                 <th scope="col">Voto</th>
                 <th scope="col">Lingua</th>
             </tr>
@@ -61,6 +65,7 @@ $films[] = $avengers;
             <tbody>
                 <tr>
                     <th scope="row"><?php echo $film->title ?></th>
+                    <td> <img src="<?php echo $film->img ?>" alt=""></td>
                     <td><?php echo $film->vote ?></td>
                     <td><?php echo $film->language ?></td>
                 </tr>
